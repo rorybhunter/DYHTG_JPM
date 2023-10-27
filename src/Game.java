17973e67a7ac9c1fc2ae367079a157942cd9d92e
“Rory <2655888H@student.gla.ac.uk>
2023-10-27 20:57:59 +0100

    Created game thread

--- src/Game.java
+++ src/Game.java
@@ -2,12 +2,31 @@ import java.awt.*;
 
 public class Game extends Canvas implements Runnable {
 
+    private boolean isRunning = false;
+    private Thread thread;
     public Game(){
         new Window(1000,550, "Spooky Puzzle Game", this);
     }
 
+    //start thread
+    private void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    //stop thread
+    private void stop(){
+        isRunning= false;
+        try{
+            thread.join();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+    // game loop
+    public void run(){
 
-    public void run(){}
+    }
     public static void main(String[] args) {
         new Game();
     }
