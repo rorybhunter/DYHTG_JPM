7ae62fdd052dca1190860da13037379c0cc44398
“Rory <2655888H@student.gla.ac.uk>
2023-10-27 21:11:06 +0100

    created gameloop

--- src/Game.java
+++ src/Game.java
@@ -24,8 +24,34 @@ public class Game extends Canvas implements Runnable {
         }
     }
     // game loop
+    // notch game loop
     public void run(){
+        this.requestFocus();
+        long lastTime = System.nanoTime(); // time since last iteration (helpful for computing delta)
+        double amountOfTicks = 60.0; // max FPS
+        double ns = 1_000_000_000 / amountOfTicks; // nanoseconds per frame
+        double delta =0; // 'progress' that must be elapsed before next frame
+        long timer = System.currentTimeMillis(); // current time, to know when next to display fps
+        int frames = 0; // frames displayed since last displayed fps
+        while (isRunning){
+            long now = System.nanoTime();
+            delta += (now-lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1){
+                tick();
+                // updates++
+                delta--;
+            }
+            render();
+            frames++;
 
+            if (System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                frames = 0;
+                // updates = 0;
+            }
+        }
+        stop();
     }
     public static void main(String[] args) {
         new Game();
