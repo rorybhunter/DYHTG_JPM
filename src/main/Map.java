1d17050ea84095af71048952aa8e147febed4bfc
“Rory <2655888H@student.gla.ac.uk>
2023-10-28 20:40:19 +0100

    added maze gen

--- src/main/Map.java
+++ src/main/Map.java
@@ -1,23 +1,24 @@
 package main;
+import MazeGenerator.MazeGenerator;
 
 import GameObjects.*;
 
 public class Map {
-    char[][] map;
+    int[][] map;
     public Map(Game game){
         Handler handler = game.getHandler();
         this.map = setNewMap();
         // create object for each element in the newly produced map and add to handler
         int i = 0, j = 0;
 
-        for (char[] row: map){
+        for (int[] row: map){
 
             j=0;
-            for (char obj : row){
-                if (obj=='p') {  //pumpkin
+            for (int obj : row){
+                if (obj=='2') {  //pumpkin
                     Pumpkin p = new Pumpkin((j*50),(i*50), ID.Pumpkin);
                     handler.addObject(p);
-                } else if (obj=='h') {
+                } else if (obj=='1') {
                     Hedge h = new Hedge((j*50),(i*50), ID.Hedge);
                     handler.addObject(h);
                 } else if (obj=='G') { //Golden pumpkin
@@ -38,31 +39,58 @@ public class Map {
             i++;
         }
 
-        //adding the player last so it apears above all of the other entities
-        Player p = new Player(1 * 50, 1 * 50, ID.Player, handler);
+        //adding the player last, so it appears above all the other entities
+        Player p = new Player(50,50, ID.Player, handler);
         handler.addObject(p);
     for (GameObject g: handler.object.stream().toList()){
         System.out.println(g.getX());
     }
     }
 
-    private char[][] setNewMap(){
+    private int[][] setNewMap() {
         // example map derived from chat gpt
         // hopefully connect this to ChatGPT to produce a new map every time
 
-        return  new char[][]  {
-                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h'},
-                {'h', 'e', 'h', 'g', 'h', 'e', 'e', 'e', 'e', 'e', 'h', 'e', 'h', 'e', 'p', 'e', 'e', 'e', 'e', 'h'},
-                {'h', 'e', 'h', 'e', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'e', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h'},
-                {'h', 'g', 'h', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'h', 'e', 'h', 'e', 'e', 'h'},
-                {'h', 'e', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'e', 'h'},
-                {'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'q', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'h'},
-                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'E', 'h'},
-                {'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'h'},
-                {'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h'},
-                {'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'h'},
-                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h'}
+//        return  new char[][]  {
+//                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h'},
+//                {'h', 'e', 'h', 'g', 'h', 'e', 'e', 'e', 'e', 'e', 'h', 'e', 'h', 'e', 'p', 'e', 'e', 'e', 'e', 'h'},
+//                {'h', 'e', 'h', 'e', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'e', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h'},
+//                {'h', 'g', 'h', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'h', 'e', 'h', 'e', 'e', 'h'},
+//                {'h', 'e', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'e', 'h'},
+//                {'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'q', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'h'},
+//                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'E', 'h'},
+//                {'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'h'},
+//                {'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h'},
+//                {'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'h'},
+//                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h'}
+//
+//        };
+//        return new char[][] {
+//                {'P', 'h', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'E'},
+//                {'e', 'h', 'e', 'h', 'e', 'e', 'e', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'e', 'h', 'e', 'e', 'e'},
+//                {'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+//                {'h', 'h', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'h'},
+//                {'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+//                {'e', 'h', 'h', 'h', 'h', 'e', 'e', 'h', 'h', 'h', 'e', 'h', 'h', 'e', 'h', 'h', 'e', 'e', 'h', 'e'},
+//                {'e', 'e', 'e', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+//                {'h', 'h', 'h', 'e', 'h', 'h', 'e', 'h', 'e', 'h', 'h', 'h', 'e', 'h', 'e', 'h', 'h', 'h', 'h', 'h'},
+//                {'e', 'e', 'e', 'e', 'h', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+//                {'h', 'h', 'e', 'h', 'h', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'h', 'h', 'e', 'e', 'e', 'e', 'h', 'e'}
+//        };
+//        return new char[][] {
+//                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h'},
+//                {'h', 'P', 'e', 'h', 'h', 'e', 'e', 'h', 'h', 'e', 'e', 'e', 'e', 'h', 'h', 'e', 'e', 'e', 'e', 'h'},
+//                {'h', 'e', 'h', 'h', 'e', 'h', 'e', 'h', 'h', 'e', 'h', 'h', 'e', 'h', 'h', 'h', 'h', 'e', 'h', 'h'},
+//                {'h', 'e', 'h', 'h', 'e', 'h', 'e', 'e', 'e', 'e', 'h', 'h', 'e', 'e', 'e', 'h', 'h', 'e', 'h', 'h'},
+//                {'h', 'e', 'h', 'h', 'e', 'e', 'h', 'h', 'h', 'h', 'e', 'e', 'e', 'h', 'e', 'h', 'h', 'e', 'h', 'h'},
+//                {'h', 'e', 'e', 'e', 'e', 'h', 'h', 'e', 'h', 'h', 'e', 'h', 'e', 'h', 'e', 'e', 'e', 'e', 'e', 'h'},
+//                {'h', 'h', 'h', 'h', 'h', 'e', 'h', 'e', 'h', 'h', 'e', 'e', 'e', 'e', 'e', 'h', 'h', 'h', 'h', 'h'},
+//                {'h', 'e', 'e', 'h', 'e', 'h', 'h', 'e', 'h', 'e', 'e', 'h', 'h', 'h', 'e', 'e', 'e', 'e', 'h', 'h'},
+//                {'h', 'h', 'e', 'h', 'e', 'e', 'e', 'e', 'e', 'h', 'e', 'e', 'h', 'e', 'e', 'h', 'h', 'e', 'h', 'h'},
+//                {'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'E', 'h', 'h'}
+//        };
 
-        };
+        MazeGenerator mazeGenerator = new MazeGenerator(15);
+        return mazeGenerator.getRawMaze();
     }
 }
