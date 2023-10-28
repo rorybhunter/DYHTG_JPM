94991f53f56e2740fbc09163fee5794105423c9e
“Rory <2655888H@student.gla.ac.uk>
2023-10-28 17:13:31 +0100

    created chest and InventoryObject, and made GoldenPumpkin an inventoryObject

--- src/GameObjects/Chest.java
+++ src/GameObjects/Chest.java
@@ -1,13 +1,17 @@
 package GameObjects;
 
+import InventoryObjects.GoldenPumpkin;
+import InventoryObjects.InventoryObject;
+
 import java.awt.*;
 
 // chest will contain items e.g. golden pumpkin. need certain amount of pumkins to leave maze.
 public class Chest  extends GameObject{
 
-
+    InventoryObject contents;
     public Chest(int x, int y, ID id) {
         super(x, y, id);
+        this.contents = new GoldenPumpkin();
     }
 
     @Override
