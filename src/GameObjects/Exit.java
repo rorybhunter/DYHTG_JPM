f2ccef105d5893c5b21bfb863c0153638449ef06
“Rory <2655888H@student.gla.ac.uk>
2023-10-29 09:46:56 +0000

    exit image added

--- src/GameObjects/Exit.java
+++ src/GameObjects/Exit.java
@@ -1,12 +1,29 @@
 package GameObjects;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Exit extends GameObject{
+    private Image exitImage;
+
     public Exit(int x, int y, ID id){
         super(x,y,id);
+        exitImage=getExitImage();
     }
-
+    private Image getExitImage(){
+        if (exitImage!=null){
+            return exitImage;
+        }else {
+            try {
+                exitImage = ImageIO.read(new File("src/images/exit.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return exitImage;
+    }
     @Override
     public void tick() {
 
@@ -14,8 +31,13 @@ public class Exit extends GameObject{
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.pink);
-        g.fillRect(x,y,50,50);
+
+        if (exitImage != null) {
+            g.drawImage(exitImage, x, y, null);
+        }else{
+            g.setColor(Color.BLACK);
+            g.fillRect(x,y,50,50);
+        }
     }
 
     @Override
